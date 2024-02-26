0297c9897e4cd7e466d329881ddb4bea150954c4
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-02-26 17:01:25 +0530

    restructured the whole code and added comments.

--- Blind_Spot_detection/Blind_Spot_detection.ino
+++ Blind_Spot_detection/Blind_Spot_detection.ino
@@ -1,116 +1,133 @@
-//miniproject on blindspot detection system.
-
-//initializing the rgb strip pins
 #include <Arduino.h>
-#define b 6
-#define g 5
-#define r 3
-//initializing the ultrasonic sensor pins
-#define echoPin 8
-#define trigPin 12
-//sensor accuracy based values for indicating distances.
-#define trigDist1 30
-#define trigDist2 10
-// buzzer pins
-#define buzzpin 13
-long duration;
-int distance;
 
-bool fade = true;
+// Pins
+#define RGB_RED_PIN 3
+#define RGB_GREEN_PIN 5
+#define RGB_BLUE_PIN 6
+#define ECHO_PIN 8
+#define TRIG_PIN 12
+#define BUZZER_PIN 13
+
+// Sensor parameters
+#define TRIGGER_DISTANCE_1 30
+#define TRIGGER_DISTANCE_2 10
+
+// LED fade parameters
+#define LED_FADE_STEP 3
+#define LED_FADE_DELAY 5
+
+// Buzzer parameters
+int BUZZER_ON_DELAY = 500;
+int BUZZER_OFF_DELAY = 500;
+
+// Function prototypes
+void initializePins();
+int measureDistance();
+void fadeRed();
+void fadeOrange();
+void fadeBlue();
+void buzz();
 
 void setup() {
-  //initializing the pins for outputs/inputs respectively.
+  // Initialize serial communication
   Serial.begin(9600);
-  pinMode(g, OUTPUT);
-  pinMode(b, OUTPUT);
-  pinMode(r, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(buzzpin, OUTPUT);
+  // Initialize pins
+  initializePins();
 }
-// calculating the distance between the host and object.
-void calDistance() {
-  digitalWrite(trigPin, LOW);
+
+void loop() {
+  // Measure distance
+  int distance = measureDistance();
+
+  // Determine LED and buzzer behavior based on distance
+  if (distance <= TRIGGER_DISTANCE_1 && distance > TRIGGER_DISTANCE_2) {
+    fadeOrange(); // Fade LED to orange
+    buzz();      // Activate buzzer
+  } else if (distance <= TRIGGER_DISTANCE_2) {
+    fadeRed();   // Fade LED to red
+    BUZZER_ON_DELAY = 100; //To make buzzer to buzz faster
+    BUZZER_OFF_DELAY = 100;
+    buzz();      // Activate buzzer
+  } else {
+    fadeBlue();  // Fade LED to blue
+    BUZZER_ON_DELAY = 1000;// to make buzzer to buzz slower
+    BUZZER_OFF_DELAY = 1000;
+    buzz();      // Activate buzzer
+  }
+}
+
+void initializePins() {
+  // Set pin modes
+  pinMode(RGB_RED_PIN, OUTPUT);
+  pinMode(RGB_GREEN_PIN, OUTPUT);
+  pinMode(RGB_BLUE_PIN, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+int measureDistance() {
+  // Send ultrasonic pulse and measure duration
+  digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
+  digitalWrite(TRIG_PIN, LOW);
+  
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  int distance = duration * 0.034 / 2;
+  
+  // Print distance for debugging
   Serial.println(distance);
-}
-//red pulsing animation to the led
-void fadedRed(int val) {
-  for (int i = 255; i > 0; i -= val) {
-    analogWrite(r, i);
-    analogWrite(g, 255);
-    analogWrite(b, 255);
-    delay(5);
-  }
-  for (int i = 0; i < 255; i += val) {
-    analogWrite(r, i);
-    analogWrite(g, 255);
-    analogWrite(b, 255);
-    delay(5);
-  }
+  return distance;
 }
 
-
-//orange color pulse to the led
-void fadedOrange(int val) {
-  for (int i = 255; i > 0; i -= val) {
-    analogWrite(r, i);
-    analogWrite(b, 255);
-    analogWrite(g, i > 230 ? i : 230);
-    delay(5);
+void fadeRed() {
+  // Fade LED to red
+  for (int i = 255; i > 0; i -= LED_FADE_STEP) {
+    analogWrite(RGB_RED_PIN, i);
+    analogWrite(RGB_GREEN_PIN, 255);
+    analogWrite(RGB_BLUE_PIN, 255);
+    delay(LED_FADE_DELAY);
   }
-  for (int i = 0; i < 255; i += val) {
-    analogWrite(r, i);
-    analogWrite(b, 255);
-    analogWrite(g, i < 230 ? 240 : i);
-    delay(5);
+  for (int i = 0; i < 255; i += LED_FADE_STEP) {
+    analogWrite(RGB_RED_PIN, i);
+    analogWrite(RGB_GREEN_PIN, 255);
+    analogWrite(RGB_BLUE_PIN, 255);
+    delay(LED_FADE_DELAY);
   }
 }
 
-//blue pulse
-void fadeInBlue(){  
- if(fade==true){   
-  for(int i=255; i>0; i-=1){     
-   analogWrite(g, 255);     
-   analogWrite(r, 255);     
-   analogWrite(b, i);      
-   delay(5);    
-  }  
- }  
- fade=false;  
- analogWrite(g, 255);  
- analogWrite(r, 255); 
- analogWrite(b, 0);
+void fadeOrange() {
+  // Fade LED to orange
+  for (int i = 255; i > 0; i -= LED_FADE_STEP) {
+    analogWrite(RGB_RED_PIN, i);
+    analogWrite(RGB_GREEN_PIN, i > 230 ? i : 230);
+    analogWrite(RGB_BLUE_PIN, 255);
+    delay(LED_FADE_DELAY);
+  }
+  for (int i = 0; i < 255; i += LED_FADE_STEP) {
+    analogWrite(RGB_RED_PIN, i);
+    analogWrite(RGB_GREEN_PIN, i < 230 ? 240 : i);
+    analogWrite(RGB_BLUE_PIN, 255);
+    delay(LED_FADE_DELAY);
+  }
 }
-//checking for distance and calling the pulse functions accordingly.
-void loop() {  
- calDistance(); 
- if(distance<=trigDist1 && distance>trigDist2){  
-  fadedOrange(3);  
-  fade=true; 
-  digitalWrite(buzzpin, HIGH);
-  delay(500);
-  digitalWrite(buzzpin, LOW);
-  delay(500);
- }
- else if(distance<=trigDist2){  
-  fadedRed(3);  
-  fade=true; digitalWrite(buzzpin, HIGH);
-  delay(50);
-  digitalWrite(buzzpin, LOW);
-  delay(50);
 
- } 
- else{  
-  fadeInBlue(); 
-  digitalWrite(buzzpin, HIGH);
-  delay(1000);
-  digitalWrite(buzzpin, LOW);
-  delay(1000);
- }
+void fadeBlue() {
+  // Fade LED to blue
+  for (int i = 255; i > 0; i -= LED_FADE_STEP) {
+    analogWrite(RGB_RED_PIN, 255);
+    analogWrite(RGB_GREEN_PIN, 255);
+    analogWrite(RGB_BLUE_PIN, i);
+    delay(LED_FADE_DELAY);
+  }
+}
+
+void buzz() {
+  // Activate buzzer
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(BUZZER_ON_DELAY);
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(BUZZER_OFF_DELAY);
 }
